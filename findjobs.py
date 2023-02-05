eebac74ee6c3ce4287cb2df4ccba4f5696b2eba5
alexRicc2 <alex.65.santanna@gmail.com>
2023-02-05 19:43:09 -0300

    class 2, scrapping data from findjobs and saving in post folder

---
+++ findjobs.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+print('Put some skill that you are not familiar with')
+unfamiliar_skill = input('>')
+print(f'filtering out {unfamiliar_skill}')
+
+def find_jobs():
+
+  html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+  soup = BeautifulSoup(html_text, 'lxml')
+  jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+  for index, job in enumerate(jobs):
+    published_date = job.find('span', class_='sim-posted').span.text
+    if 'few' in published_date:
+      company_name = job.find('h3', class_="joblist-comp-name").text
+      skills = job.find('span', class_="srp-skills").text.replace(' ', '')
+      more_info = job.header.h2.a['href']
+      if unfamiliar_skill not in skills:
+        with open(f'posts/{index}.txt', 'w') as f:
+
+          f.write(f'Company name: {company_name.strip()} \n')
+          f.write(f'Required skills: {skills.strip()} \n')
+          f.write(f'More info: {more_info}')
+        print(f'file saved: {index}') 
+    
+if __name__ == '__main__':
+  while True:
+    find_jobs()
+    print(f'waiting ten minutes')
+    time.sleep(600)
